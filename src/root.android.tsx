f86c54a598d3142483a3b80d19800d48060e382f
Archheretic <setervang@hotmail.com>
2017-04-17 23:49:23 +0200

    Change GUI items now aligned to left instead of center + some extra padding.

--- src/root.android.tsx
+++ src/root.android.tsx
@@ -97,13 +97,15 @@ const styles: any = StyleSheet.create({
     container: {
         flex: 1,
         // justifyContent: "center",
-        alignItems: "center",
+        // alignItems: "center",
+        paddingLeft: 20,
         backgroundColor: "#F5FCFF",
     },
     title: {
         fontSize: 24,
         color: "#000000",
-        margin: 10,
+        marginBottom: 10,
+        marginTop: 10,
         fontWeight: "bold"
     },
     parkingLotName: {
