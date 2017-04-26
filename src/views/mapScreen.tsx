3161be745ec1042620a6babe5d315b964edaf0d4
Archheretic <setervang@hotmail.com>
2017-04-26 12:46:41 +0200

    add google map integration and geolocation of the user based on gps position.

--- src/views/mapScreen.tsx
+++ src/views/mapScreen.tsx
@@ -1,36 +1,93 @@
 /**
  * Created by archheretic on 19.04.17.
  *
- * Error: Element type is invalid: expected a string (for built-in components) or a class/function
- * (for composoite components) but got: undefined. Check the render method for `MapScreen`.
+ * Solution for finding correct latitude- and longitude delta was inspired by
+ * http://stackoverflow.com/questions/30606827/set-the-bounds-of-a-mapview
  */
 import * as React from "react";
 import {
     StyleSheet,
-    View,
-    Text,
-    ViewStyle,
     ImageStyle
 } from "react-native";
-//import * as nativeMaps from 'react-native-maps';
-//import {MapView} from nativeMaps;
 
-const MapView = require('react-native-maps');
+const MapView = require("react-native-maps");
 
 export interface Props {
     navigation: any;
 }
 
-export interface State {}
+export interface State {
+    currentRegion: {
+        latitude: number,
+        longitude: number,
+        latitudeDelta: number,
+        longitudeDelta: number
+    }
+}
+
+const earthRadiusInKM = 6371;
+// you can customize these two values based on your needs
+const radiusInKM = 0.5;
+const aspectRatio = 1;
 
 export class MapScreen extends React.Component<Props, State> {
     constructor(props) {
         super(props);
+        this.state = {
+            currentRegion: {
+                // standard values used if no gps data is found, these should probably be changed...
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }
+        }
     }
-    //
-    // render() {
-    //     return <Text>hei</Text>rr
-    // }
+
+    watchID: number = null;
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const radiusInRad = radiusInKM / earthRadiusInKM;
+                const longitudeDelta = rad2deg(radiusInRad / Math.cos(deg2rad(latitude)));
+                const latitudeDelta = aspectRatio * rad2deg(radiusInRad);
+
+               // const region = calculateRegion(position);
+                this.setState({currentRegion: {
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: latitudeDelta,
+                    longitudeDelta: longitudeDelta
+                }});
+            },
+            (error) => alert(error.message),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+        );
+        this.watchID = navigator.geolocation.watchPosition((position) => {
+            console.log(position);
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const radiusInRad = radiusInKM / earthRadiusInKM;
+            const longitudeDelta = rad2deg(radiusInRad / Math.cos(deg2rad(latitude)));
+            const latitudeDelta = aspectRatio * rad2deg(radiusInRad);
+            this.setState({currentRegion: {
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: latitudeDelta,
+                longitudeDelta: longitudeDelta
+            }});
+        })
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID);
+    }
+
+
     render() {
         console.log("MapView:");
 
@@ -38,12 +95,8 @@ export class MapScreen extends React.Component<Props, State> {
         return (
             <MapView
                 style={styles.container}
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
+                initialRegion={this.state.currentRegion}
+                showsUserLocation = {true}
             />
         );
 
@@ -62,3 +115,12 @@ const styles = StyleSheet.create<Style>({
         right: 0,
         bottom: 0,  }
 });
+
+function deg2rad (angle) {
+    return angle * 0.017453292519943295 // (angle / 180) * Math.PI;
+}
+
+function rad2deg (angle) {
+    return angle * 57.29577951308232 // angle / Math.PI * 180
+}
+
